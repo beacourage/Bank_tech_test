6a380d5d2206e47bcc14ecdf1f647f10309d226b
beatricecourage <bea_courage@hotmail.co.uk>
2019-02-13 12:02:15 +0000

    Tests passing, initial credit of zero, responds to credit

--- lib/individualTransactions.rb
+++ lib/individualTransactions.rb
@@ -1,10 +1,12 @@
 class IndividualTransactions
 
 INITIAL_BALANCE = 0
+INITIAL_CREDIT = 0
 
-  attr_accessor :total_balance
+  attr_accessor :total_balance, :credit
 
-  def initialize (total_balance = INITIAL_BALANCE)
+  def initialize (total_balance = INITIAL_BALANCE, credit = INITIAL_CREDIT)
     @total_balance = total_balance
+    @credit = credit
   end
 end
--- spec/individualTransactions_spec.rb
+++ spec/individualTransactions_spec.rb
@@ -2,22 +2,31 @@ require 'individualTransactions'
 
   describe IndividualTransactions do
   INITIAL_BALANCE = IndividualTransactions::INITIAL_BALANCE
+  INITIAL_CREDIT = IndividualTransactions::INITIAL_CREDIT
+    describe "#totalbalance" do
+      it "responds to total_balance" do
+        transaction = IndividualTransactions.new
+        expect(transaction).to respond_to(:total_balance)
+      end
 
-    describe "#initialbalance" do
       it "There is an initial balance of 0" do
         transaction = IndividualTransactions.new
         expect(transaction.total_balance).to eq(INITIAL_BALANCE)
       end
 
-      it "responds to total_balance" do
-        transaction = IndividualTransactions.new
-        expect(transaction).to respond_to(:total_balance)
-      end
     end
 
 
+    describe "#credit" do
+      it "responds to credit" do
+        transaction = IndividualTransactions.new
+        expect(transaction).to respond_to(:credit)
+      end
 
-
-
+      it "There is initial credit of 0" do
+        transaction = IndividualTransactions.new
+        expect(transaction.credit).to eq(INITIAL_CREDIT)
+      end
+    end
 
   end
